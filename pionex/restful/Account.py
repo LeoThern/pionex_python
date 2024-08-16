3f0f2702cfae38d1197c45eaba7799a400bc0b51
Leo Thern <47295238+LeoThern@users.noreply.github.com>
2024-08-16 17:24:22 +0200

    rename

--- pionex/restful/Account.py
+++
@@ -1,8 +0,0 @@
-from pionex.internal.RestClient import RestClient
-
-class Account(RestClient):
-    def __init__(self, key, secret):
-        super().__init__(key, secret)
-
-    def get_balance(self):
-        return self._send_request('GET', '/api/v1/account/balances')
